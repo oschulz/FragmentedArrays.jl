3d3feca1e67235cc4f6b39ca68aaf3cff7647084
Oliver Schulz <oschulz@mpp.mpg.de>
2017-10-22 09:50:46 +0200

    Require Julia v0.6, update CI configs, prepare for Julia v0.7

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 # This file is a part of FragmentedArrays.jl, licensed under the MIT License (MIT).
 
-@Base.Test.testset "Package FragmentedArrays" begin
+import Compat.Test
+Test.@testset "Package FragmentedArrays" begin
     include.([
     ])
 end
